35fd19bed166fc1d24c587b1eb359427e7ffe563
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-08 15:11:26 +0100

    insertright

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -  inserts a node on the right
+ * @parent: pointer node
+ * @value: node value
+ *
+ * Return: a pointer to the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_right_node;
+if (!parent)
+return (NULL);
+new_right_node = malloc(sizeof(binary_tree_t));
+if (!new_right_node)
+return (NULL);
+
+if (parent->left == NULL)
+{
+new_right_node->parent = parent;
+new_right_node->left = parent->left;
+new_right_node->right = NULL;
+new_right_node->n = value;
+}
+else
+parent->left = new_right_node;
+
+return (new_right_node);
+}
