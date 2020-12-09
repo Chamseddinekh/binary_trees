f1868f81eb7c8f960835e515b7c577f670316a7b
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-09 02:28:18 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,8 +6,7 @@
  * @second: pointer to the second node
  * Return: 0
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 
 if (first == NULL || second == NULL)
