78e57463635686d7e81bbcb31a186b6008adaf31
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-09 02:32:00 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
 
 if (first == NULL && second == NULL)
