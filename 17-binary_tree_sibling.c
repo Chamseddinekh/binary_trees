5e0efaf7e13814c9ef1b32dfb689db196dfa92b7
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-10 22:01:28 +0100

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 if (!node || node->parent == NULL)
 return (NULL);
 
-if(node->parent->left == node)
+if (node->parent->left == node)
 return (node->parent->right);
 return (node);
 
