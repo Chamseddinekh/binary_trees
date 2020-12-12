faf6ae86c8f1c7e57d3159102a15207592291087
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-12 14:14:15 +0100

    header

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
