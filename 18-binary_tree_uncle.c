40f500f93a2d63ecec8e30e472d38516abeed811
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-14 13:45:42 +0100

    header

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (!node || node->parent->parent == NULL)
+if (!node || node->parent->parent == NULL || node->parent == NULL)
 return (NULL);
 
 if (node->parent->parent)
