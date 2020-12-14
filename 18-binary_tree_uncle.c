35cc2130d5cee25195e35f2a47fc8c6e5c1fd1f6
chamseddine <chamseddine.khedhri@gmail.com>
2020-12-14 14:52:58 +0100

    unclefixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,13 +8,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+
 if (!node || node->parent->parent == NULL || node->parent == NULL)
 return (NULL);
 
-if (node->parent->parent)
+if (node->parent->parent->left == node->parent)
 return (node->parent->parent->right);
-
-if (node->parent->parent)
+if (node->parent->parent->right == node->parent)
 return (node->parent->parent->left);
 
 return (NULL);
